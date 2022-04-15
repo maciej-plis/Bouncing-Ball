b2d55a6a7aac3cadf65f021a2907c96e23a2f2f9
Maciej <maciej.plis.secure@gmail.com>
2022-04-15 11:12:03 +0200

    Game Screen refactor

--- ball-bouncing/core/src/com/matthias/GameScreen.java
+++ ball-bouncing/core/src/com/matthias/GameScreen.java
@@ -2,86 +2,65 @@ package com.matthias;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
-import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import static com.badlogic.gdx.graphics.Color.PURPLE;
+import static com.badlogic.gdx.graphics.Color.*;
 import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Filled;
+import static com.badlogic.gdx.utils.Align.center;
 
 public class GameScreen extends ScreenAdapter {
 
-    BallBouncingGame game;
-    Circle ball;
+    private final BallBouncingGame game;
+    private final Ball ball;
 
-    float xVel;
-    float yVel;
+    private final InputProcessor gameScreenInputProcessor = new InputAdapter() {
+        @Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            Vector2 clickedPoint = new Vector2(screenX, Gdx.graphics.getHeight() - screenY);
+            ball.bounceUp(clickedPoint);
+            return true;
+        }
+    };
 
-    int bounces;
-
-    public GameScreen(BallBouncingGame ballBouncing) {
-        this.game = ballBouncing;
+    public GameScreen(BallBouncingGame game) {
+        this.game = game;
+        this.ball = new Ball(
+            new Vector2(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f),
+            65,
+            PURPLE,
+            new GlyphLayout(this.game.font, "Hello World!", WHITE, 0, center, false)
+        );
     }
 
     @Override
     public void show() {
-        bounces = 0;
-
-        xVel = 0f;
-        yVel = 0f;
-
-        ball = new Circle(new Vector2(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f), 65);
-
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                if(ball.contains(screenX, Gdx.graphics.getHeight() - screenY)) {
-                    yVel += 600;
-                    float multiplier = (ball.x - screenX) / 65;
-                    xVel = 600 * multiplier;
-                    bounces++;
-                }
-
-                return true;
-            }
-        });
+        Gdx.input.setInputProcessor(gameScreenInputProcessor);
     }
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0, 0, 0, 1);
+        ScreenUtils.clear(BLACK);
 
-        yVel = yVel > -300 ? yVel - 300 * delta : -300;
+        ball.update(0, Gdx.graphics.getWidth(), delta);
 
-        ball.setX(ball.x + xVel * delta);
-        ball.setY(ball.y + yVel * delta);
-
-        if (ball.x - 65 < 0) {
-            xVel *= -1;
-            ball.x = 65;
-        } else if(ball.x + 65 > Gdx.graphics.getWidth()) {
-            xVel *= -1;
-            ball.x = Gdx.graphics.getWidth() - 65;
+        if (ball.isBelowCamera(0)) {
+            game.setScreen(new GameOverScreen(game, ball.getBounces()));
         }
 
-        if(ball.y + 65 < 0) {
-            game.setScreen(new GameOverScreen(game, bounces));
-        }
-
+        // Draw ball
         game.shapeRenderer.begin(Filled);
-        game.shapeRenderer.setColor(PURPLE);
-        game.shapeRenderer.circle(ball.x, ball.y, ball.radius);
+        game.shapeRenderer.setColor(ball.getColor());
+        game.shapeRenderer.circle(ball.getX(), ball.getY(), ball.getRadius());
         game.shapeRenderer.end();
 
-
+        // Draw bounce counter
         game.batch.begin();
-        GlyphLayout layout = new GlyphLayout(game.font, "Hello World!");
-        float x = ball.x - layout.width / 2;
-        float y = ball.y + layout.height / 2;
-        game.font.draw(game.batch, layout, x, y);
-        game.font.draw(game.batch, "Bounces: " + bounces, 10, Gdx.graphics.getHeight() - 10);
+        game.font.draw(game.batch, ball.getBallText(), ball.getX(), ball.getY());
+        game.font.draw(game.batch, "Bounces: " + ball.getBounces(), 10, Gdx.graphics.getHeight() - 10);
         game.batch.end();
     }
 
